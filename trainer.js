81432ff53a186829cfeace7dbdf27baa2057ac8f
Max <m.loesch@hotmail.de>
2020-08-08 19:47:56 +0200

    Koreanisch funktioniert

--- trainer.js
+++ trainer.js
@@ -6,22 +6,99 @@ function getDisplaySpan() {
     return document.querySelector('#display span');
 }
 
-function getAdditionalDisplay() {
-    return document.querySelector('#additionalDisplay span');
+function getanswerDisplay() {
+    return document.querySelector('#answerDisplay span');
 }
 
 function newNumber() {
     let displaySpan = getDisplaySpan();
     let numberFromInput = document.querySelector('#numberFromInput');
     let numberToInput = document.querySelector('#numberToInput');
-    let additionaldisplay = getAdditionalDisplay();
+    let answerDisplay = getanswerDisplay();
 
     let numberFromUserInput = Number.parseInt(numberFromInput.value);
     let numberFrom = isNaN(numberFromUserInput) ? STANDARD_FROM : numberFromUserInput;
     let numberToUserInput = Number.parseInt(numberToInput.value);
     let numberTo = isNaN(numberToUserInput) ? STANDARD_TO : numberToUserInput;
-    displaySpan.innerHTML = Math.floor(numberFrom + (Math.random() * (numberTo - numberFrom + 1)));
-    additionaldisplay.innerHTML = Math.random() > 0.5 ? "Chinesisch!" : "Koreanisch!";
+    let randomNumber = Math.floor(numberFrom + (Math.random() * (numberTo - numberFrom + 1)));
+    displaySpan.innerHTML = randomNumber;
+    let isKoreanNumber = document.querySelector('#koreanNumber').checked;
+    try {
+        answerDisplay.innerHTML = isKoreanNumber ? getNumberWrittenKorean(randomNumber) : getNumberWrittenChinese(randomNumber);
+        hideAnswer();
+    } catch (e) {
+        answerDisplay.innerHTML = e.message;
+    }
+}
+
+
+const numbersWrittenChinese = {
+    "chinese": {
+        "0": {
+            "1": "일",
+            "2": "이",
+            "3": "삼",
+            "4": "사",
+            "5": "오",
+            "6": "육",
+            "7": "칠",
+            "8": "팔",
+            "9": "구"
+        },
+        "1": "십",
+        "2": "백",
+        "3": "천"
+    }
+};
+const numbersWrittenKorean = {
+    0: {
+        "0": "",
+        "1": "하나",
+        "2": "둘",
+        "3": "셋",
+        "4": "넷",
+        "5": "다섯",
+        "6": "여섯",
+        "7": "일곱",
+        "8": "여덟",
+        "9": "아홉",
+    },
+    1: {
+        "1": "열",
+        "2": "스물",
+        "3": "서른",
+        "4": "마흔",
+        "5": "쉰",
+        "6": "예순",
+        "7": "일흔",
+        "8": "여든",
+        "9": "아흔"
+    }
+
+};
+
+
+function getNumberWrittenKorean(number) {
+    if (number > 99) {
+        throw new Error("Fehler! Nur Zahlen < 99 im Koreanischen System.");
+    }
+    const numberAsStringReversed = reverseString(number.toString());
+
+    var output = "";
+    for (i = 0; i < numberAsStringReversed.length; i++) {
+        output = numbersWrittenKorean[i][numberAsStringReversed.charAt(i)] + " " + output;
+    }
+
+    return output;
+
+}
+
+function getNumberWrittenChinese(number) {
+
+}
+
+function reverseString(str) {
+    return str.split("").reverse().join("");
 }
 
 function getWeekDays() {
@@ -37,20 +114,20 @@ function getWeekDays() {
 }
 
 function newWeekDay() {
-    let additionaldisplay = getAdditionalDisplay();
+    let answerDisplay = getanswerDisplay();
     let displaySpan = getDisplaySpan();
     let weekdays = getWeekDays();
     let random = Math.floor(Math.random() * 7);
     let randomWeekDayGerman = Object.keys(weekdays)[random];
     displaySpan.innerHTML = randomWeekDayGerman;
-    additionaldisplay.innerHTML = weekdays[randomWeekDayGerman];
-    hideWeekDayAnswer();
+    answerDisplay.innerHTML = weekdays[randomWeekDayGerman];
+    hideAnswer();
 }
 
-function hideWeekDayAnswer() {
-    getAdditionalDisplay().setAttribute('style', 'background-color: black')
+function hideAnswer() {
+    getanswerDisplay().setAttribute('style', 'background-color: black')
 }
 
-function showWeekDayAnswer() {
-    getAdditionalDisplay().setAttribute('style', 'background-color: white')
+function showAnswer() {
+    getanswerDisplay().setAttribute('style', 'background-color: white')
 }
